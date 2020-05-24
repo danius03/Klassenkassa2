d84756666805002fdc8c0fef747abc4c33d289ee
daniel <daniel.lehner030303@gmail.com>
2020-05-24 10:56:46 +0200

    ActionBar der Main- und 2.Activity und ActionMenus. und Fehlerverbesserungen im Manifest 15min

--- app/src/main/java/com/example/klassenkassa/activities/Activity2.java
+++ app/src/main/java/com/example/klassenkassa/activities/Activity2.java
@@ -1,8 +1,12 @@
 package com.example.klassenkassa.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.klassenkassa.R;
 
@@ -12,5 +16,58 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        configActionBar();
+    }
+
+    private void configActionBar()
+    {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.action_menue,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id=item.getItemId();
+
+        switch(id) {
+            case R.id.new_student:
+                createNewStudent();
+                break;
+            case R.id.save_students:
+                saveStudents();
+                break;
+            case R.id.load_students:
+                loadStudents();
+                break;
+            case android.R.id.home:
+                Intent data=new Intent(this, MainActivity.class);
+                setResult(RESULT_OK, data);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadStudents()
+    {
+
+    }
+
+    private void saveStudents()
+    {
+
+    }
+
+    private void createNewStudent()
+    {
+
     }
 }
--- app/src/main/java/com/example/klassenkassa/activities/MainActivity.java
+++ app/src/main/java/com/example/klassenkassa/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.klassenkassa.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.klassenkassa.R;
 
@@ -12,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.action_menue,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id=item.getItemId();
+
+        switch(id) {
+            case R.id.new_category:
+                createNewCategory();
+                break;
+            case R.id.settings:
+                showSettings();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createNewCategory()
+    {
+
+    }
+
+    private void showSettings()
+    {
+
     }
 }
