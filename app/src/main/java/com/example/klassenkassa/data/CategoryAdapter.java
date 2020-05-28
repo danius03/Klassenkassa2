0f54bbf7075fa70148055abd67ee1b6f9503aeb5
daniel <daniel.lehner030303@gmail.com>
2020-05-28 10:23:55 +0200

    Adapter vervollständigt und Namenskonventionsfehler behoben 15min

--- app/src/main/java/com/example/klassenkassa/data/CategoryAdapter.java
+++ app/src/main/java/com/example/klassenkassa/data/CategoryAdapter.java
@@ -1,18 +1,38 @@
 package com.example.klassenkassa.data;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.klassenkassa.R;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class CategoryAdapter extends BaseAdapter {
+
+    private List<Category> categories;
+    private int layoutId;
+    private LayoutInflater inflater;
+
+    public CategoryAdapter(Context ctx, int layoutId, List<Category> categories) {
+        this.categories = categories;
+        this.layoutId = layoutId;
+        inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return categories.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return categories.get(position);
     }
 
     @Override
@@ -22,6 +42,10 @@ public class CategoryAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        Category c= categories.get(position);
+        View listItem = (convertView == null) ?inflater.inflate(this.layoutId, null) : convertView;
+        ((TextView) (listItem.findViewById(R.id.categoryName_textView))).setText(c.getName());
+        ((TextView) (listItem.findViewById(R.id.dueDate_textView))).setText(c.getDueDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        return listItem;
     }
 }
--- app/src/main/java/com/example/klassenkassa/data/StudentAdapter.java
+++ app/src/main/java/com/example/klassenkassa/data/StudentAdapter.java
@@ -1,18 +1,35 @@
 package com.example.klassenkassa.data;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.klassenkassa.R;
+
+import java.util.List;
 
 public class StudentAdapter extends BaseAdapter {
+    private List<Student> students;
+    private int layoutId;
+    private LayoutInflater inflater;
+
+    public StudentAdapter(Context ctx, int layoutId, List<Student> students) {
+        this.students = students;
+        this.layoutId = layoutId;
+        inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return students.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return students.get(position);
     }
 
     @Override
@@ -22,6 +39,12 @@ public class StudentAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        Student s = students.get(position);
+        View listItem = (convertView == null) ?inflater.inflate(this.layoutId, null) : convertView;
+        ((TextView) (listItem.findViewById(R.id.studentName_textView))).setText(s.getLastname()+" "+s.getFirstname());
+        ((TextView) (listItem.findViewById(R.id.studentNumber_textView))).setText(s.getStudentID());
+        ((TextView) (listItem.findViewById(R.id.studentValues_textView))).setText(s.getCost()+"");
+        ((TextView) (listItem.findViewById(R.id.studentStatus_textView))).setText(s.getStatus()+"");
+        return listItem;
     }
 }
